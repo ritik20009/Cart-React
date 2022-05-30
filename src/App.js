3afbfb7e2588de7512da2b492e7a9bd2963a560c
ritik raj <ritikozib189@gmail.com>
2022-05-30 21:08:37 +0530

    added the increase,decrease,delete functionalities

--- src/App.js
+++ src/App.js
@@ -1,10 +1,115 @@
-import CartItem from './CartItem'
-function App() {
-  return (
-    <div className="App">
-      <CartItem />
-    </div>
-  );
+import React from 'react';
+import Cart from './Cart';
+import Navbar from './Navbar';
+
+class App extends React.Component {
+
+  constructor () {
+    super();
+    this.state = {
+      products: [
+        {
+          price: 5999,
+          title: 'Watch',
+          qty: 0,
+          img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388',
+          id: 1
+        },
+        {
+          price: 59999,
+          title: 'Mobile Phone',
+          qty: 0,
+          img: 'https://images.unsplash.com/photo-1589492477829-5e65395b66cc?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387',
+          id: 2
+        },
+        {
+          price: 149999,
+          title: 'Laptop',
+          qty: 0,
+          img: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171',
+          id: 3
+        }
+      ]
+    }
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
+    // this.testing();
+  }
+  handleIncreaseQuantity = (product) => {
+    console.log('Heyy please inc the qty of ', product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    products[index].qty += 1;
+
+    this.setState({
+      products
+    })
+  }
+  handleDecreaseQuantity = (product) => {
+    console.log('Heyy please inc the qty of ', product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    if (products[index].qty === 0) {
+      return;
+    }
+
+    products[index].qty -= 1;
+
+    this.setState({
+      products
+    })
+  }
+  handleDeleteProduct = (id) => {
+    const { products } = this.state;
+
+    const items = products.filter((item) => item.id !== id); // [{}]
+
+    this.setState({
+      products: items
+    })
+  }
+
+  getCartCount = () => {
+    const { products } = this.state;
+
+    let count = 0;
+
+    products.forEach((product) => {
+      count += product.qty;
+    })
+
+    return count;
+  }
+
+  getCartTotal = () =>{
+    const {products} = this.state;
+    let carttotal=0;
+
+    products.map((product) => {
+
+      carttotal = carttotal + product.qty * product.price;
+    })
+    return carttotal;
+  }
+
+  render () {
+    const { products } = this.state;
+    return (
+      <div className="App">
+        <Navbar count={this.getCartCount()} />
+        <Cart
+          products={products}
+          onIncreaseQuantity={this.handleIncreaseQuantity}
+          onDecreaseQuantity={this.handleDecreaseQuantity}
+          onDeleteProduct={this.handleDeleteProduct}
+        />
+        <div  style = {{padding: 10, fontSize: 20}}>
+            TOTAL: {this.getCartTotal()}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,80 +1,55 @@
-import React from 'react'
+import React from 'react';
 
-class CartItem extends React.Component {
-
-    constructor() {
-        super();
-        this.state = {
-            price: 999,
-            title: 'Mobile Phone',
-            qty: 1,
-            img: ''
-        }
-    }
-    increaseQuantity = () => {
-       // console.log('this' , this.state);
-        // set state form 1
-        // this.setState({
-        //     qty: this.state.qty + 1
-        // });
-
-        // set state form 2
-        // if previous state required
-
-        this.setState((prevState) => {
-            return {
-                qty: prevState.qty + 1
-            }
-        });
-    }
-
-    decreaseQuantity = () =>{
-
-        if(this.state.qty==0){
-            return;
-        }
-        else{
-            this.setState((prevState) =>{
-                return {
-                    qty: prevState.qty - 1
-                }
-            });
-        }
-    }
-    render() {
-        const {price,title,qty} = this.state;
-        return (
-            <div className="cart-item">
-                <div className="left-block">
-                    <img style = {styles.image} />
-                </div>
-                <div className="right-block">
-                    <div style={{fontSize: 25}}>{title}</div>
-                    <div style={{color: '#777'}}>RS: {price}</div>
-                    <div style={{color: '#777'}}>Qty: {qty}</div>
-                </div>
-                <div className="cart-item-actions">
-                    {/* Buttons */}
-                    <img alt="increase" className="action-icons" src="https://cdn-icons.flaticon.com/png/512/3024/premium/3024515.png?token=exp=1652715521~hmac=d543e1ddd019b1c69cc1a1e65148774b" onClick={this.increaseQuantity}/>
-                    <img alt="decrease" className="action-icons" src="https://cdn-icons.flaticon.com/png/512/2740/premium/2740679.png?token=exp=1652715478~hmac=4dce72a4326d6308300f70205367fb21" onClick = {this.decreaseQuantity}/>
-                    <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png" />
-                </div>
-            </div>
-        );
-    }
+const CartItem = (props) => {
+  const { price, title, qty } = props.product;
+  const {
+    product,
+    onIncreaseQuantity,
+    onDecreaseQuantity,
+    onDeleteProduct
+  } = props;
+  return (
+    <div className="cart-item">
+      <div className="left-block">
+        <img style={styles.image} src = {product.img} alt=""/>
+      </div>
+      <div className="right-block">
+        <div style={ { fontSize: 25 } }>{title}</div>
+        <div style={ { color: '#777' } }>Rs {price} </div>
+        <div style={ { color: '#777' } }>Qty: {qty} </div>
+        <div className="cart-item-actions">
+          {/* Buttons */}
+          <img
+            alt="increase"
+            className="action-icons"
+            src="https://as1.ftcdn.net/v2/jpg/02/51/03/82/1000_F_251038282_CLb3d8tk99bGoU9ILEYS8vY215fgRmGT.jpg"
+            onClick={() => onIncreaseQuantity(product)}
+          />
+          <img
+            alt="decrease"
+            className="action-icons"
+            src="https://as2.ftcdn.net/v2/jpg/02/78/84/57/1000_F_278845758_9xl3srVgd8p4jquxgxugGaHV1e5EOlLO.jpg"
+            onClick={() => onDecreaseQuantity(product)}
+          />
+          <img
+            alt="delete"
+            className="action-icons"
+            src="https://cdn-icons.flaticon.com/png/512/484/premium/484662.png?token=exp=1653560349~hmac=37d2251f904d171dc2da0d7992b07b61"
+            onClick={() => onDeleteProduct(product.id)}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
-
-
-
 
 const styles = {
-    image: {
-        height: 110,
-        width: 110,
-        borderRadius: 4,
-        background: '#ccc'
-    }
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: '#ccc'
+  }
 }
-
 
 export default CartItem;
